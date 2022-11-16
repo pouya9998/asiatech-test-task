92dbffc3434bea5c8dd23bc7db1ea6499043ed83
PouyaHeydari <pouya9998@gmail.com>
2022-11-16 12:42:33 +0330

    order relations added

--- Modules/Order/Entities/Order.php
+++ Modules/Order/Entities/Order.php
@@ -2,11 +2,23 @@
 
 namespace Modules\Order\Entities;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Food\Entities\Food;
 
 class Order extends Model
 {
 
     protected $guarded = [];
+
+    public function food()
+    {
+        return $this->belongsTo(Food::class)->withDefault();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->withDefault();
+    }
 
 }
